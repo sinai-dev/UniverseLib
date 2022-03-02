a920dd0eee763a8b9cfe238983ea307fa444d01f
Sinai <49360850+sinai-dev@users.noreply.github.com>
2022-03-03 04:45:40 +1100

    Bump version

--- src/Universe.cs
+++ src/Universe.cs
@@ -23,7 +23,7 @@ namespace UniverseLib
         }
 
         public const string NAME = "UniverseLib";
-        public const string VERSION = "1.2.7";
+        public const string VERSION = "1.2.8";
         public const string AUTHOR = "Sinai";
         public const string GUID = "com.sinai.universelib";
 
