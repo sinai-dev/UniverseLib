3fb564e0cae2731d947019fd54d84e2d1ab4d6d0
R. Beltran <rbeltran8000@gmail.com>
2022-03-04 14:57:55 +0100

    provide easy access for 3rd party plugins to UnityExplorer's Inspector

--- src/Utility/MiscUtility.cs
+++ src/Utility/MiscUtility.cs
@@ -8,6 +8,10 @@ namespace UniverseLib.Utility
 {
     public static class MiscUtility
     {
+        private static bool isFirstCallToInspect = true;
+        private static Type inspectorManagerType;
+        private static Type cacheObjectBaseType;
+        
         /// <summary>
         /// Check if a string contains another string, case-insensitive.
         /// </summary>
@@ -42,6 +46,43 @@ namespace UniverseLib.Utility
                     return false;
             }
             return true;
+        }
+
+        /// <summary>
+        /// Tells whether UnityExplorer plugin is currently loaded or not.
+        /// </summary>
+        /// <returns></returns>
+        public static bool CanInspect() => !isFirstCallToInspect
+            ? inspectorManagerType != null
+            : (isFirstCallToInspect = false) !=
+              ((inspectorManagerType = ReflectionUtility.GetTypeByName("UnityExplorer.InspectorManager")) != null);
+
+        /// <summary>
+        /// Sends a type to open in a new tab of UnityExplorer's Inspector, if available.
+        /// </summary>
+        /// <param name="type">a Type to inspect</param>
+        public static void Inspect(Type type)
+        {
+            if (!CanInspect())
+                return;
+
+            inspectorManagerType.GetMethod("Inspect", ReflectionUtility.FLAGS, null, 
+                new Type[] {typeof(Type)}, null)?.Invoke(null, new object[] {type});
+        }
+
+        /// <summary>
+        /// Sends an object to UnityExplorer's Inspector, if available.
+        /// </summary>
+        /// <param name="obj">an object to inspect</param>
+        public static void Inspect(object obj, object sourceCache = null)
+        {
+            if (!CanInspect())
+                return;
+            
+            cacheObjectBaseType ??= ReflectionUtility.GetTypeByName("UnityExplorer.CacheObject.CacheObjectBase");
+
+            inspectorManagerType.GetMethod("Inspect", ReflectionUtility.FLAGS, null,
+                new Type[] {typeof(object), cacheObjectBaseType}, null)?.Invoke(null, new object[] {obj, sourceCache});
         }
     }
 }
