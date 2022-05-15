d2da1dc723e529f4a8dc91ce7726a9225510c4ac
Sinai <49360850+sinai-dev@users.noreply.github.com>
2022-05-15 18:56:18 +1000

    Update Universe.cs

--- src/Universe.cs
+++ src/Universe.cs
@@ -23,7 +23,7 @@ namespace UniverseLib
         }
 
         public const string NAME = "UniverseLib";
-        public const string VERSION = "1.4.1";
+        public const string VERSION = "1.4.2";
         public const string AUTHOR = "Sinai";
         public const string GUID = "com.sinai.universelib";
 
